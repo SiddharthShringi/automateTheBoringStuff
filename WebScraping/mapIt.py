89e52725b4dd9df2d21ba8db6ae61c0a05b75c61
BlackSamsung <siddharthshringi@gmail.com>
2019-02-27 18:44:49 +0530

    chapter 11 of the book - Web Scraping

---
+++ WebScraping/mapIt.py
@@ -0,0 +1,6 @@
+# Required Modules
+#  - webbrowser. Comes with Python and opens a browser to a specific page.
+#  - Requests. Downloads files and web pages from the Internet.
+#  - Beautiful Soup. Parse HTML, the format that web pages are written in.
+#  - Selenium. Lanunches and controls a web browser. Selenium is able to fill in
+#    forms and simulate mouse clicks in this browser.
